069d27b2562afd43be7c967a19f99faf911d8ce3
Keno Fischer <kfischer@college.harvard.edu>
2013-08-13 03:24:52 -0400

    Also remove PM initialization

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -3517,7 +3517,6 @@ static void init_julia_llvm_env(Module *m)
 
     // set up optimization passes
     FPM = new FunctionPassManager(jl_Module);
-    PM = new PassManager();
 
     
 #ifdef LLVM32
